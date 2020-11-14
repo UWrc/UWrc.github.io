2c1984c30ae353c1aa8f6b2048fe41f1401a9efc
Jerry Lin <linj1064@gmail.com>
2020-11-13 20:49:52 -0800

    scale slot machine text size by viewport width + css refactor

--- src/pages/components/Stats/StatSlotMachineItem.jsx
+++ src/pages/components/Stats/StatSlotMachineItem.jsx
@@ -4,27 +4,6 @@ import useInView from "react-cool-inview";
 import PropTypes from "prop-types";
 import styles from "./styles.module.css"
 
-const REEL_THEME = {
-  reel: {
-    height: "1em",
-    display: "flex",
-    alignItems: "flex-end",
-    overflowY: "hidden",
-    fontSize: "64px",
-    fontWeight: "300",
-    color: "white",
-    lineHeight: "0.95em"
-  },
-  group: {
-    transitionDelay: "0ms",
-    transitionTimingFunction: "ease-in-out",
-    transform: "translate(0, 0)",
-    height: "1em"
-  },
-  number: {
-    height: "1em"
-  }
-};
 
 StatSlotMachineItem.propTypes = {
   value: PropTypes.string.isRequired,
@@ -37,7 +16,7 @@ export default function StatSlotMachineItem(props) {
     unobserveOnEnter: true
   })
 
-  let reel = inView ? <Reel text={props.value} theme={REEL_THEME} /> : <></>
+  let reel = inView ? <Reel text={props.value} theme={styles} /> : <></>
 
   return <div className="col-4" ref={ref}>
     <div
