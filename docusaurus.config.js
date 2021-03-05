c7e29f1f99590726d9a2c74fd9004f98a0b0227b
Nam Pho <npho@uw.edu>
2021-03-05 14:03:25 -0800

    testing netlify deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'HYAK',
   tagline: 'Powering discoveries in every field',
-  url: 'https://hyak.netlify.app',
+  url: 'https://hyak.uw.edu',
   baseUrl: '/',
   favicon: 'img/logos/uw_icon.png',
   organizationName: 'UWrc', // Github org/user name
