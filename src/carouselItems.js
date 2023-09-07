315eec8520383c6db2f35830fc35fd34125981d4
mjuwit <45882046+mjuwit@users.noreply.github.com>
2023-09-07 08:48:51 -0700

    Update carouselItems.js
    Front page slide links all go back to https://hyak.uw.edu/ (links were mostly broken)

--- src/carouselItems.js
+++ src/carouselItems.js
@@ -3,24 +3,24 @@ export const CAROUSEL_ITEMS = [
     image: '/img/carousel/protein.jpg',
     title: 'Molecular Dynamics',
     caption: 'MD is one of the biggest use cases for HPC.',
-    linkUrl: '/blog/2021/01/09/gromacs-gpu',
+    linkUrl: 'https://hyak.uw.edu/',
   },
   {
     image: '/img/carousel/network.jpg',
     title: 'Computational Social Sciences',
     caption: 'HPC is increasingly used in the social sciences.',
-    linkUrl: 'https://www.washington.edu/uwit/partnerships-2019/building-better-online-communities/',
+    linkUrl: 'https://hyak.uw.edu/',
   },
   {
     image: '/img/carousel/containers.jpg',
     title: 'Research Containers',
     caption: 'We provide support for computational reproducibility and portability.',
-    linkUrl: '/docs/tools/containers'
+    linkUrl: 'https://hyak.uw.edu/'
   },
   {
     image: '/img/carousel/neuron.jpg',
     title: 'Machine Learning',
     caption: 'We have tools and training to get you started with deep learning.',
-    linkUrl: '/blog/2021/01/10/pytorch-cuda11',
+    linkUrl: 'https://hyak.uw.edu/',
   }
 ]
