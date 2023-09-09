501efa5a00f2585b8e7e1f86ea070639648a121b
Nam Pho <npho@uw.edu>
2023-09-09 16:52:20 -0400

    remove all carousel links

--- src/carouselItems.js
+++ src/carouselItems.js
@@ -3,24 +3,20 @@ export const CAROUSEL_ITEMS = [
     image: '/img/carousel/protein.jpg',
     title: 'Molecular Dynamics',
     caption: 'MD is one of the biggest use cases for HPC.',
-    linkUrl: 'https://hyak.uw.edu/',
   },
   {
     image: '/img/carousel/network.jpg',
     title: 'Computational Social Sciences',
     caption: 'HPC is increasingly used in the social sciences.',
-    linkUrl: 'https://hyak.uw.edu/',
   },
   {
     image: '/img/carousel/containers.jpg',
     title: 'Research Containers',
     caption: 'We provide support for computational reproducibility and portability.',
-    linkUrl: 'https://hyak.uw.edu/'
   },
   {
     image: '/img/carousel/neuron.jpg',
     title: 'Machine Learning',
     caption: 'We have tools and training to get you started with deep learning.',
-    linkUrl: 'https://hyak.uw.edu/',
   }
 ]
