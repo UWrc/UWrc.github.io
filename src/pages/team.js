54383bf9d7a5c8870917e07eb6810a83b249f6bc
Kristen N. Finch <finchkn@uw.edu>
2026-01-02 15:50:17 -0800

    redirect fix, docusaurus style.

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Team() {
   // Redirects /storage page to the Knowledge Base article
-  window.location.href = 'https://it.uw.edu/uw-it/divisions/research-computing/';
+  // window.location.href = 'https://it.uw.edu/uw-it/divisions/research-computing/';
   return (
       <Layout title="Team">
           <div class="container">
-            Redirecting       
+            Redirecting     
+            <BrowserOnly>
+                  {window.location.href = 'https://it.uw.edu/uw-it/divisions/research-computing/'}
+            </BrowserOnly>   
           </div>
       </Layout>
   );
