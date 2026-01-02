024b2a73467c64747d65ae0c2a332f1ed0d86e22
Kristen N. Finch <finchkn@uw.edu>
2026-01-02 13:23:58 -0800

    adding redirects for out of date pages still appearing on Google Searches.

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,78 +7,13 @@ import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 
 export default function About() {
+  // Redirects /storage page to the Knowledge Base article
+  window.location.href = 'https://it.uw.edu/guides/research/';
   return (
-<Layout title="About">
-  <div class="container">
-    <div class="row">
-      <br />
-    </div>
-    <Splash />
-    <Tabs
-      className="unique-tabs"
-      defaultValue="apple"
-      values={[
-        {label: 'Apple', value: 'apple'},
-        {label: 'Orange', value: 'orange'},
-        {label: 'Carrot', value: 'carrot'},
-      ]}>
-      <TabItem value="apple">This is an apple 🍎</TabItem>
-      <TabItem value="orange">This is an orange 🍊</TabItem>
-      <TabItem value="carrot">This is a carrot 🥕</TabItem>
-    </Tabs>
-
-
-    <div class="row">
-      <p>
-      How to buy in.
-      </p>
-
-    </div>
-
-    <div class="row">
-      
-      <div class="col">
-        <div class="card-demo">
-          <div class="card">
-            <div class="card__header">
-              <h3>Lorem Ipsum</h3>
-            </div>
-            <div class="card__body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-                suspendisse ultrices gravida.
-              </p>
-            </div>
-            <div class="card__footer">
-              <button class="button button--secondary button--block">See All</button>
-            </div>
+      <Layout title="About">
+          <div class="container">
+            Redirecting       
           </div>
-        </div>
-      </div>
-      
-      <div class="col">
-        <div class="card-demo">
-          <div class="card">
-            <div class="card__header">
-              <h3>Lorem Ipsum</h3>
-            </div>
-            <div class="card__body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-                suspendisse ultrices gravida.
-              </p>
-            </div>
-            <div class="card__footer">
-              <button class="button button--secondary button--block">See All</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    
-    </div>
-  </div>
-</Layout>
+      </Layout>
   );
 }
--- src/pages/systems.js
+++ src/pages/systems.js
@@ -7,137 +7,14 @@ import StatContainer from "./components/Stats/StatContainer";
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 
-export default function systems() {
+export default function Systems() {
+  // Redirects /storage page to the Knowledge Base article
+  window.location.href = '/docs/klone/architecture';
   return (
-<Layout title="Hyak">
-  <br />
-  <h3 style={{textAlign: "center"}}>Hyak</h3>
-  
-  <StatContainer statItems={PageContent.STAT_ITEMS} />
-
-  <div class="container">  
-  <br />
-    <div class="row">
-      <p>
-        Hyak is part of the University of Washington's cyberinfrastructure plan to support world-class research in every department. Hyak is an ecosystem of high-performance compute (HPC) clusters and supporting infrastructure (e.g., data management, computational training, scientific consulting). Acknowlegding our Pacific Northwest heritage, Hyak means "fast" in Chinook jargon. Counting 1, 2, and 3 translates to "ikt", "mox", and "klone" in this local trading language. The numbers are cluster names corresponding to the generation of Hyak cluster deployed. We are currently on the 3rd generation cluster for Hyak. The University of Washington acknowledges the Coast Salish peoples of this land, the land which touches the shared waters of all tribes and bands within the Suquamish, Tulalip and Muckleshoot nations.
-      </p>
-    </div>
-
-    <div class="row">
-      <Tabs
-        className="unique-tabs"
-        defaultValue="klone"
-        values={[
-          {label: '1. ikt', value: 'ikt'},
-          {label: '2. mox', value: 'mox'},
-          {label: '3. klone', value: 'klone'},
-        ]}>
-        <TabItem value="ikt">
-        <table>
-            <tr>
-              <td align="right"></td><td><img style={{width: "300px", height: "200px"}} src="/img/systems/ikt.jpg" alt="Ikt cluster image"/></td>
-            </tr>
-            <tr>
-              <td align="right">Cluster</td><td><code>ikt</code></td>
-            </tr>
-            <tr>
-              <td align="right">Service</td><td>October 2010 — June 2020</td>
-            </tr>
-            <tr>
-              <td align="right">Operating System</td><td>CentOS 6</td>
-            </tr>            
-            <tr>
-              <td align="right">Compute</td><td>8,008 CPU cores [Broadwell]</td>
-            </tr>
-            <tr>
-              <td align="right">Accelerators</td><td>47 GPU cards [Tesla M2075]</td>
-            </tr>
-            <tr>
-              <td align="right">Interconnect</td><td>10 Gbps [Myri-10G Myrinet]</td>
-            </tr>
-            <tr>
-              <td align="right">Topology</td><td>Non-blocking 1-layer fat tree</td>
-            </tr>
-            <tr>
-              <td align="right">Storage</td><td>1.0 PB GPFS</td>
-            </tr>
-            <tr>
-              <td align="right" style={{verticalAlign: "top"}}>Namesake</td><td>Ikt translates to 1 in Chinook trading jargon and was the name of the 1st Hyak cluster. <code>ikt</code> is no longer in service. Reported figures represent best records.</td>
-            </tr>
-          </table>
-        </TabItem>
-        <TabItem value="mox">
-          <table>
-            <tr>
-              <td align="right"></td><td><img style={{width: "300px", height: "200px"}} src="/img/systems/mox.jpg" alt="Mox cluster image"/></td>
-            </tr>
-            <tr>
-              <td align="right">Cluster</td><td><code>mox</code></td>
-            </tr>
-            <tr>
-              <td align="right">Service</td><td>June 2018 — January 2025</td>
-            </tr>
-            <tr>
-              <td align="right">Operating System</td><td>CentOS 7</td>
-            </tr>
-            <tr>
-              <td align="right">Compute</td><td>18,376 CPU cores [Cascade Lake]</td>
-            </tr>
-            <tr>
-              <td align="right" style={{verticalAlign: "top"}}>Accelerators</td><td>112 GPU cards [Turing]<br />20 Xeon PHI</td>
-            </tr>
-            <tr>
-              <td align="right">Interconnect</td><td>100 Gbps [OPA100 Omnipath]</td>
-            </tr>
-            <tr>
-              <td align="right">Topology</td><td>Non-blocking 1-layer fat tree</td>
-            </tr>
-            <tr>
-              <td align="right">Storage</td><td>1.0 PB GPFS</td>
-            </tr>
-            <tr>
-              <td align="right" style={{verticalAlign: "top"}}>Namesake</td><td>Mox translates to 2 in Chinook trading jargon and is the name of the 2nd Hyak cluster. <code>mox</code> is active but no longer accepting node additions and not growing. Reported figures represent peak capacity during its service.</td>
-            </tr>
-          </table>
-        </TabItem>
-        <TabItem value="klone">
-          <table>
-            <tr>
-              <td align="right"></td><td><img style={{width: "300px", height: "200px"}} src="/img/systems/klone.jpg" alt="Klone cluster image"/></td>
-            </tr>
-            <tr>
-              <td align="right">Cluster</td><td><code>klone</code></td>
-            </tr>
-            <tr>
-              <td align="right">Service</td><td>January 2021 — Present</td>
-            </tr>
-            <tr>
-              <td align="right">Operating System</td><td>Rocky 8</td>
-            </tr>
-            <tr>
-              <td align="right">Compute</td><td>44,184 CPU cores [AMD Genoa, Intel Cascade Lake, Intel Ice Lake]</td>
-            </tr>
-            <tr>
-              <td align="right">Accelerators</td><td>954 GPU cards [Turing, Ampere, Hopper]</td>
-            </tr>
-            <tr>
-              <td align="right">Interconnect</td><td>100 Gbps [HDR100 Infiniband]</td>
-            </tr>
-            <tr>
-              <td align="right">Topology</td><td>3:1 blocking 2-layer fat tree</td>
-            </tr>
-            <tr>
-              <td align="right" style={{verticalAlign: "top"}}>Storage</td><td>2.7 PB GPFS</td>
-            </tr>
-            <tr>
-              <td align="right" style={{verticalAlign: "top"}}>Namesake</td><td>Klone translates to 3 in Chinook trading jargon and is the name of the 3rd Hyak cluster. <code>klone</code> is active and scheduled for a soft launch in Q1 2021 and a full launch by Q2 2021. Reported figures are updated as the cluster continues to grow.</td>
-            </tr>
-          </table>
-        </TabItem>
-      </Tabs>
-    </div>
-    <br />
-  </div>
-</Layout>
+      <Layout title="Systems">
+          <div class="container">
+            Redirecting       
+          </div>
+      </Layout>
   );
 }
